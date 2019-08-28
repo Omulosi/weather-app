648fb53ab4ed51df5db699a9d4b1c723e97b4d35
Omulosi <mulongojohnpaul@gmail.com>
2019-08-28 04:56:33 +0300

    Updates

---
+++ precache-manifest.0879e2bad52d23bbf7b8e1ccae3c1d42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9531009865d11b405f9435d226b7ea5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ff569b46328ccb5977f2",
+    "url": "/weather-app/static/css/main.3b1485df.chunk.css"
+  },
+  {
+    "revision": "0611c3ee4731284b47f5",
+    "url": "/weather-app/static/js/2.96c37c9d.chunk.js"
+  },
+  {
+    "revision": "ff569b46328ccb5977f2",
+    "url": "/weather-app/static/js/main.8f5cbb22.chunk.js"
+  },
+  {
+    "revision": "52387862e54d9be38456",
+    "url": "/weather-app/static/js/runtime~main.68dbe4ab.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
